4593baaef7148ab9b675ec06150780ffcaed0146
Nathan Faucett <nathanfaucett@gmail.com>
2015-06-16 18:48:40 -0500

    updates

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 var isLength = require("is_length"),
+    isFunction = require("is_function"),
     isObjectLike = require("is_object_like");
 
 
 module.exports = isArrayLike;
 
 
-function isArrayLike(obj) {
-    return isObjectLike(obj) && isLength(obj.length);
+function isArrayLike(value) {
+    return isObjectLike(value) && isLength(value.length) && !isFunction(value);
 }
